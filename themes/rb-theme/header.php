2a5c78a9e61d6d6ff22ff2c0fab197ca40e87b02
Mimmi <mimmizlnilsson@gmail.com>
2023-01-13 19:02:34 +0100

    wip: navbar

--- themes/rb-theme/header.php
+++ themes/rb-theme/header.php
@@ -29,30 +29,21 @@ $site_url = network_site_url('/');
 		<header id="menu" class="absolute top-0 left-0 z-50 w-full">
 
 			<!-- Main Navigation -->
-			<nav id="navbar" class="fixed w-full text-white bg-black lg:py-7 lg:h-auto"> <!-- Nav 'container' -->
-
-				<div class="relative z-50 w-full border-b border-white lg:hidden h-[62px]">
-					<button class="absolute w-12 h-12 text-white top-3 right-2 lg:right-0 lg:top-0 navbar-burger">
-						<span aria-hidden="true" class="absolute block w-12 h-1 transition duration-500 ease-in-out transform bg-current"></span>
-						<span aria-hidden="true" class="absolute block w-12 h-1 transition duration-300 ease-in-out transform bg-current"></span>
-						<span aria-hidden="true" class="absolute block w-12 h-1 transition duration-500 ease-in-out transform bg-current"></span>
-					</button>
-
-					<div class="block w-[1px] h-[62px] bg-white absolute left-[62px] top-0"></div>
-					<div class="block w-[1px] h-[62px] bg-white absolute right-[62px] top-0"></div>
-
-					<div class="absolute cursor-pointer left-3 top-3">
-						<a href="<?php echo $site_url; ?>"><img class="w-12" src=<?php echo $root_uri . '/resources/images/rb-logo-clear.svg'; ?> /></a>
+			<nav id="navbar" class="fixed w-full text-white bg-black lg:py-5 lg:h-auto "> <!-- Nav 'container' -->
+				<div class="relative w-full md:max-w-[1040px] m-auto h-[100px] bg-lime">
+					<div class="absolute hidden cursor-pointer left-4 top-4 lg:top-0 lg:left-0 lg:relative lg:block">
+						<a href="<?php echo $site_url; ?>"><img class="w-[62px] m-auto border-2 border-black " src=<?php echo $root_uri . '/resources/images/rb-logo-clear.svg'; ?> /></a>
 					</div>
+
 				</div>
 
 				<div class="fixed left-0 z-40 w-full h-[50%] transition-transform bg-gray top-62 lg:top-0 lg:bg-transparent lg:h-auto lg:w-auto lg:relative navbar-menu lg:block">
 					<div class="items-center p-12 mt-10 ml-2 justify-evenly navbar-backdrop lg:-mt-0 lg:ml-0 lg:border-none lg:flex lg:p-0 lg:w-1/2">
 
 
-
+						<!-- Logo reg nav -->
 						<div class="absolute hidden cursor-pointer left-4 top-4 lg:top-0 lg:left-10 lg:relative lg:block">
-							<a href="<?php echo $site_url; ?>"><img class="w-2/12" src=<?php echo $root_uri . '/resources/images/rb-logo-clear.svg'; ?> /></a>
+							<a href="<?php echo $site_url; ?>"><img class="w-1/12" src=<?php echo $root_uri . '/resources/images/rb-logo-clear.svg'; ?> /></a>
 						</div>
 
 						<?php wp_nav_menu([
@@ -60,25 +51,16 @@ $site_url = network_site_url('/');
 							'container_class' => 'header-menu'
 						]); ?>
 
-
-
-
-
 						<!-- Hamburger Nav --->
 						<div class="lg:hidden">
-
-
-
-
+							<div class="mt-20">
+								<!-- 'Footer' items -->
+							</div>
 						</div>
 					</div>
 				</div>
-
 			</nav>
-
-
 		</header>
-
 		<div id="content">
 			<?php do_action('amoba_content_start'); ?>
 			<main>
--- themes/rb-theme/resources/js/app.js
+++ themes/rb-theme/resources/js/app.js
@@ -1,2 +1,31 @@
-// const { active } = require("browser-sync");
-jQuery(document).ready(function ($) {});
+// // const { active } = require("browser-sync");
+
+// window.onload = function () {
+//   const navbar = document.getElementById("navbar");
+//   const scrollTop = navbar.offsetTop;
+//   var prevScrollpos = window.pageYOffset;
+
+//   function menuAnimation() {
+//     var currentScrollPos = window.pageYOffset;
+//     let opened = document.querySelector(".navbar-open");
+
+//     if (prevScrollpos > currentScrollPos && !opened) {
+//       navbar.style.top = "0";
+//       scrollUp();
+//     } else if (!opened) {
+//       navbar.style.top = "-150px";
+//     }
+
+//     prevScrollpos = currentScrollPos;
+//   }
+
+//   function hamburger() {
+//     const burger = document.querySelector(".navbar-burger-icon");
+
+//     function burgerClick(e) {
+//       document.body.classList.toggle("navbar-open");
+//     }
+
+//     burger.addEventListener("click", burgerClick);
+//   }
+// }
