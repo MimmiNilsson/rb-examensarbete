f8160e067a7d9b81b835b9889996144af3469353
Mimmi <mimmizlnilsson@gmail.com>
2023-01-14 21:32:20 +0100

    wip

--- themes/rb-theme/header.php
+++ themes/rb-theme/header.php
@@ -30,82 +30,80 @@ $site_url = network_site_url('/');
 
 			<!-- Main Navigation -->
 			<nav id="navbar" class="fixed w-full md:py-7 md:bg-gray md:h-auto menu-bg"> <!-- md:bg-transparent -->
+				<div class="relative z-50 w-full border-b-2 border-white md:hidden h-[62px] bg-gray">
+					<button class="absolute w-12 h-12 text-white top-2.5 right-3 md:right-0 md:top-0 navbar-burger">
+						<span aria-hidden="true" class="absolute block w-12 h-1 transition duration-500 ease-in-out transform bg-orange"></span>
+						<span aria-hidden="true" class="absolute block w-12 h-1 transition duration-300 ease-in-out transform bg-orange"></span>
+						<span aria-hidden="true" class="absolute block w-12 h-1 transition duration-500 ease-in-out transform bg-orange"></span>
+					</button>
 
-<div class="relative z-50 w-full border-b-2 border-white md:hidden h-[62px] bg-gray md:bg-none">
-	<button class="absolute w-12 h-12 text-white top-2.5 right-3 md:right-0 md:top-0 navbar-burger">
-		<span aria-hidden="true" class="absolute block w-12 h-1 transition duration-500 ease-in-out transform bg-orange"></span>
-		<span aria-hidden="true" class="absolute block w-12 h-1 transition duration-300 ease-in-out transform bg-orange"></span>
-		<span aria-hidden="true" class="absolute block w-12 h-1 transition duration-500 ease-in-out transform bg-orange"></span>
-	</button>
+					<div class="block w-[2px] h-[62px] bg-white absolute left-[62px] top-0"></div>
+					<div class="block w-[2px] h-[62px] bg-white absolute right-[62px] top-0"></div>
 
-	<div class="block w-[2px] h-[62px] bg-white absolute left-[62px] top-0"></div>
-	<div class="block w-[2px] h-[62px] bg-white absolute right-[62px] top-0"></div>
-
-	<div class="absolute cursor-pointer top-1.5 left-2">
-		<a href="<?php echo $site_url; ?>"><img class="w-[52px]" src=<?php echo $root_uri . '/resources/images/rb-logo-clear.svg'; ?> /></a>
-	</div>
-	<div class="absolute cursor-pointer top-1.5 left-20"> PLACEHOLDER
-		<!-- <a href="<?php echo $site_url; ?>"><img class="w-[52px]" src=<?php echo $root_uri . '/resources/images/rb-logo-clear.svg'; ?> /></a> -->
-	</div>
-</div>
-
-<div class="fixed top-0 left-0 z-40 w-full h-screen transition-transform bg-gray md:h-auto md:w-auto md:relative navbar-menu md:block"> <!-- md:bg-transparent -->
-	<div class="relative items-center justify-between p-12 mt-10 ml-2 border-2 navbar-backdrop md:-mt-0 md:ml-0 md:border-none md:flex md:p-0 border-blue ">
-
-		<?php wp_nav_menu([
-			'theme_location' => 'menu_left',
-			'container_class' => 'header-menu-left header-menu'
-		]); ?>
-
-		<div class="absolute hidden cursor-pointer left-4 top-4 md:top-0 md:left-0 md:relative md:block">
-			<a href="<?php echo $site_url; ?>"><img class="w-[72px]" src=<?php echo $root_uri . '/resources/images/rb-logo-clear.svg'; ?> /></a>
-		</div>
-
-		<?php wp_nav_menu([
-
-			'theme_location' => 'menu_right',
-			'container_class' => 'header-menu-right header-menu'
-		]); ?>
-
-		<!-- Hamburger Nav --->
-		<!-- <div class="lg:hidden">
-
-			<div class="mt-20">
-
-				<?php if (!empty($instagram_link)) : ?>
-					<a class="flex items-start mt-6" href="<?php the_field('instagram_link', 'option'); ?>" target="_blank">
-						<img src=<?php echo $root_uri . "/images/instagram-logo.svg" ?>>
-						<span class="mt-1 ml-2 typo-submenu text-[12px]">Instagram</span>
-					</a>
-				<?php endif; ?>
-				<?php if (!empty($facebook_link)) : ?>
-					<a class="flex items-start pr-6 mt-2" href="<?php the_field('facebook_link', 'option'); ?>" target="_blank">
-						<img src=<?php echo $root_uri . "/images/facebook-logo.svg" ?>>
-						<span class="mt-1 ml-2 typo-submenu text-[12px]">Facebook</span>
-					</a>
-				<?php endif; ?>
-			</div>
-			<div class="flex flex-row justify-between mt-12 lg:hidden typo-body text-[16px] md:text-[18px]">
-				<div class="w-[45%]">
-					<div class="flex">
-						<a href=""></a>
-						<?php if (!empty($telefonnummer)) : the_field('telefonnummer', 'option');
-						endif; ?>
+					<div class="absolute cursor-pointer top-1.5 left-2">
+						<a href="<?php echo $site_url; ?>"><img class="w-[52px]" src=<?php echo $root_uri . '/resources/images/rb-logo-clear.svg'; ?> /></a>
 					</div>
-					<?php if (!empty($email)) : the_field('email', 'option');
-					endif; ?>
+					<div class="absolute cursor-pointer top-1.5 left-20"> PLACEHOLDER
+						<!-- <a href="<?php echo $site_url; ?>"><img class="w-[52px]" src=<?php echo $root_uri . '/resources/images/rb-logo-clear.svg'; ?> /></a> -->
+					</div>
 				</div>
-				<div class="w-[45%]">
-					<?php if (!empty($adress)) : the_field('adress', 'option');
-					endif; ?>
+
+				<div class="fixed top-0 left-0 z-40 w-full h-screen transition-transform bg-gray md:h-auto md:w-auto md:relative navbar-menu md:block"> <!-- md:bg-transparent -->
+					<div class="relative items-center justify-between p-12 mt-10 ml-2 border-2 navbar-backdrop md:-mt-0 md:ml-0 md:border-none md:flex md:p-0 border-blue">
+
+						<?php wp_nav_menu([
+							'theme_location' => 'menu_left',
+							'container_class' => 'header-menu-left header-menu'
+						]); ?>
+
+						<div class="absolute hidden cursor-pointer left-4 top-4 md:top-0 md:left-0 md:relative md:block">
+							<a href="<?php echo $site_url; ?>"><img class="w-[72px]" src=<?php echo $root_uri . '/resources/images/rb-logo-clear.svg'; ?> /></a>
+						</div>
+
+						<?php wp_nav_menu([
+
+							'theme_location' => 'menu_right',
+							'container_class' => 'header-menu-right header-menu'
+						]); ?>
+
+						<!-- Hamburger Nav --->
+						<!-- <div class="lg:hidden">
+
+							<div class="mt-20">
+
+								<?php if (!empty($instagram_link)) : ?>
+									<a class="flex items-start mt-6" href="<?php the_field('instagram_link', 'option'); ?>" target="_blank">
+										<img src=<?php echo $root_uri . "/images/instagram-logo.svg" ?>>
+										<span class="mt-1 ml-2 typo-submenu text-[12px]">Instagram</span>
+									</a>
+								<?php endif; ?>
+								<?php if (!empty($facebook_link)) : ?>
+									<a class="flex items-start pr-6 mt-2" href="<?php the_field('facebook_link', 'option'); ?>" target="_blank">
+										<img src=<?php echo $root_uri . "/images/facebook-logo.svg" ?>>
+										<span class="mt-1 ml-2 typo-submenu text-[12px]">Facebook</span>
+									</a>
+								<?php endif; ?>
+							</div>
+							<div class="flex flex-row justify-between mt-12 lg:hidden typo-body text-[16px] md:text-[18px]">
+								<div class="w-[45%]">
+									<div class="flex">
+										<a href=""></a>
+										<?php if (!empty($telefonnummer)) : the_field('telefonnummer', 'option');
+										endif; ?>
+									</div>
+									<?php if (!empty($email)) : the_field('email', 'option');
+									endif; ?>
+								</div>
+								<div class="w-[45%]">
+									<?php if (!empty($adress)) : the_field('adress', 'option');
+									endif; ?>
+								</div>
+							</div>
+
+						</div> -->
+					</div>
 				</div>
-			</div>
-
-		</div> -->
-	</div>
-</div>
-
-</nav>
+			</nav>
 		</header>
 		<div id="content">
 			<?php do_action('amoba_content_start'); ?>
