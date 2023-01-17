2ff61214f28acef451226c2350039b7895d04055
Mimmi <mimmizlnilsson@gmail.com>
2023-01-17 22:15:50 +0100

    wip

--- themes/rb-theme/template-parts/blocks/selected-cases.php
+++ themes/rb-theme/template-parts/blocks/selected-cases.php
@@ -39,7 +39,7 @@ $image_size = 'large';
 
 <!-- FEATURED CASES -->
 <div class="m-auto lg:w-[90%] xl:w-[1220px] relative mt-4 mb-9 md:mb-24">
-  <div class="grid h-full gap-4 mx-6 border-2 border-black md:grid-cols-3 lg:mx-0">
+  <div class="grid h-full gap-4 mx-6 md:grid-cols-3 lg:mx-0">
 
     <!-- CASE 1 -->
     <div class="relative h-full p-6 md:p-2 bg-gray box-shadow">
@@ -108,89 +108,3 @@ $image_size = 'large';
     </div>
   </div>
 </div>
-
-
-
-
-
-
-
-
-
-
-
-
-<!-- FEATURED CASES -->
-<!-- <div class="m-auto lg:w-[90%] xl:w-[1220px] relative mt-4 mb-9 md:mb-24 border-2 border-blue">
-  <div class="grid h-full gap-2 mx-6 border-2 border-black md:grid-cols-2 lg:grid-cols-3 lg:mx-0"> -->
-
-<!-- CASE 1 -->
-<!-- <div class="relative h-full p-10 bg-gray md:p-14 lg:p-20">
-      <img class="border-2 border-red-600 " src="<?php echo wp_get_attachment_image_url($image_1_id, $image_size); ?>" />
-
-      <?php if (!empty($title_1)) : ?>
-        <p class=""><?php echo $title_1; ?></p>
-      <?php endif; ?>
-
-      <?php if (!empty($customer_1)) : ?>
-        <p class=""><?php echo $customer_1; ?></p>
-      <?php endif; ?>
-
-      <?php
-      if ($link_1) :
-        $link_1_url = $link_1['url'];
-        $link_1_title = $link_1['title'];
-        $link_1_target = $link_1['target'] ? $link_1['target'] : '_self';
-      ?>
-        <a class="link" href="<?php echo esc_url($link_1_url); ?>" target="<?php echo esc_attr($link_1_target); ?>"> <?php echo esc_html($link_1_title); ?></a>
-      <?php endif; ?>
-
-    </div> -->
-
-<!-- CASE 2 -->
-<!-- <div class="relative h-full p-10 mb-3 sm:mb-0 bg-gray md:p-14 lg:p-20">
-      <img class="border-2 border-red-600" src="<?php echo wp_get_attachment_image_url($image_2_id, $image_size); ?>" />
-
-      <?php if (!empty($title_2)) : ?>
-        <p class=""><?php echo $title_2; ?></p>
-      <?php endif; ?>
-
-      <?php if (!empty($customer_2)) : ?>
-        <p class=""><?php echo $customer_2; ?></p>
-      <?php endif; ?>
-
-      <?php
-      if ($link_2) :
-        $link_2_url = $link_2['url'];
-        $link_2_title = $link_2['title'];
-        $link_2_target = $link_2['target'] ? $link_2['target'] : '_self';
-      ?>
-        <a class="link" href="<?php echo esc_url($link_2_url); ?>" target="<?php echo esc_attr($link_2_target); ?>"> <?php echo esc_html($link_1_title); ?></a>
-      <?php endif; ?>
-
-    </div> -->
-
-<!-- CASE 3 -->
-<!-- <div class="relative h-full p-10 mb-3 sm:mb-0 bg-gray md:p-14 lg:p-20">
-      <img class="border-2 border-red-600" src="<?php echo wp_get_attachment_image_url($image_3_id, $image_size); ?>" />
-
-      <?php if (!empty($title_3)) : ?>
-        <p class=""><?php echo $title_3; ?></p>
-      <?php endif; ?>
-
-      <?php if (!empty($customer_3)) : ?>
-        <p class=""><?php echo $customer_3; ?></p>
-      <?php endif; ?>
-
-      <?php
-      if ($link_3) :
-        $link_3_url = $link_3['url'];
-        $link_3_title = $link_3['title'];
-        $link_3_target = $link_3['target'] ? $link_3['target'] : '_self';
-      ?>
-        <a class="link" href="<?php echo esc_url($link_3_url); ?>" target="<?php echo esc_attr($link_3_target); ?>"> <?php echo esc_html($link_3_title); ?></a>
-      <?php endif; ?>
-
-    </div>
-  </div>
-</div> -->
