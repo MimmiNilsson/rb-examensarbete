44f28626c6547dec599322d65a1ddd762c21b8bc
Mimmi <mimmizlnilsson@gmail.com>
2023-01-21 16:01:16 +0100

    feat: secondary (woo) hero acf

--- themes/rb-theme/functions.php
+++ themes/rb-theme/functions.php
@@ -199,6 +199,20 @@ function acf_init_block_types()
 				'mode' => false
 			)
 		));
+
+		// Secondary (Woo) Hero Block
+		acf_register_block_type(array(
+			'name'              => 'secondary-hero',
+			'title'             => __('RB | Secondary (Woo) Hero'),
+			'render_template'   => 'template-parts/blocks/secondary-hero.php',
+			'category'          => 'custom',
+			'icon'              => 'cover-image',
+			'keywords'          => array('secondary', 'hero', 'woo'),
+			'mode' => 'edit',
+			'supports' => array(
+				'mode' => false
+			)
+		));
 	}
 }
 
@@ -275,20 +289,21 @@ add_filter('document_title_parts', function ($title) {
 // WOOCOMMERCE RELATED
 
 // Remove "Select options" button from (variable) products on the main WooCommerce shop page.
-add_filter( 'woocommerce_loop_add_to_cart_link', function( $product ) {
+add_filter('woocommerce_loop_add_to_cart_link', function ($product) {
 	global $product;
-	if ( is_shop() && 'variable' === $product->product_type ) {
+	if (is_shop() && 'variable' === $product->product_type) {
 		return '';
 	} else {
-		sprintf( '<a href="%s" data-quantity="%s" class="%s" %s>%s</a>',
-			esc_url( $product->add_to_cart_url() ),
-			esc_attr( isset( $args['quantity'] ) ? $args['quantity'] : 1 ),
-			esc_attr( isset( $args['class'] ) ? $args['class'] : 'button' ),
-			isset( $args['attributes'] ) ? wc_implode_html_attributes( $args['attributes'] ) : '',
-			esc_html( $product->add_to_cart_text() )
+		sprintf(
+			'<a href="%s" data-quantity="%s" class="%s" %s>%s</a>',
+			esc_url($product->add_to_cart_url()),
+			esc_attr(isset($args['quantity']) ? $args['quantity'] : 1),
+			esc_attr(isset($args['class']) ? $args['class'] : 'button'),
+			isset($args['attributes']) ? wc_implode_html_attributes($args['attributes']) : '',
+			esc_html($product->add_to_cart_text())
 		);
 	}
-} );
+});
 
 // add_action('woocommerce_before_single_product', 'add_breadcrumb', 20);
 // function add_breadcrumb(){
