73c2f7080f7e23081799a9b641d265ed4e9b8318
Mimmi <mimmizlnilsson@gmail.com>
2023-01-26 12:23:33 +0100

    chore: code cleaning

--- themes/rb-theme/page.php
+++ themes/rb-theme/page.php
@@ -1,14 +1,3 @@
-<?php // if (is_page('cart') || is_page('my-account') || is_page('checkout')) : 
-?>
-<?php // get_header('black'); 
-?>
-<?php // else : 
-?>
-<?php // get_header(); 
-?>
-<?php // endif; 
-?>
-
 <?php if (is_page('content-single-product')) : ?>
 	<?php get_header('black'); ?>
 <?php else : ?>
@@ -21,6 +10,3 @@
 
 <?php
 get_footer();
-
-// about, cases, contact, home, my-account, cart all display this page
-// shop and product pages do not... 
