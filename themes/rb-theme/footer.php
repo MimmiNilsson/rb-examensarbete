98e210bac7def2a4a9b44dd478a87a84bfb0a941
Mimmi <mimmizlnilsson@gmail.com>
2023-01-26 11:58:39 +0100

    updated: footer styling

--- themes/rb-theme/footer.php
+++ themes/rb-theme/footer.php
@@ -13,36 +13,31 @@ $address = get_field('address', 'option');
 ?>
 
 <?php if (!is_page('my-account')) : ?>
-  <footer id="footer" class="grid text-center text-white bg-black justify-items-center mt-9 md:mt-24">
-    <div class="w-full h-[5px] grid grid-cols-4">
-      <div class="w-full h-full bg-purple"></div>
-      <div class="w-full h-full bg-blue"></div>
-      <div class="w-full h-full bg-orange"></div>
-      <div class="w-full h-full bg-lime"></div>
-    </div>
-
-    <div class="w-[67px] mt-9 md:mt-24 mb-4">
-      <a href="<?php echo $site_url; ?>"><img src=<?php echo $theme_uri . "/images/rb-logo-clear.svg" ?>> </a>
-    </div>
-
-    <div class="flex typo-subheading">
-      <div class="mb-9 md:mb-24">
-        <div class="mb-3">
-          <?php if (!empty($email)) : ?>
-            <a href="mailto:<?php echo $email; ?>"><?php echo $email; ?></a>
-          <?php endif; ?>
+  <footer id="footer" class="text-white bg-black mt-9 md:mt-24 h-[25vh] flex justify-center items-center flex-col">
+    <div class="w-[90%] xl:w-[1220px] px-6 lg:px-0">
+      <div class="flex flex-col-reverse items-center justify-between sm:flex-row">
+        <div class="w-[83px] content-center align-self mt-2 sm:mt-0">
+          <a href="<?php echo $site_url; ?>"><img src=<?php echo $theme_uri . "/images/rb-logo-clear.svg" ?>> </a>
         </div>
 
-        <div class="mb-3">
-          <?php if (!empty($phone)) : ?>
-            <a href="tel:<?php echo $phone; ?>"><?php echo $phone; ?></a>
-          <?php endif; ?>
-        </div>
+        <div class="flex flex-col md:w-[80%] w-full sm:items-end items-center">
+          <div class="mb-4 text-center typo-subheading">
+            <?php if (!empty($email)) : ?>
+              <a href="mailto:<?php echo $email; ?>"><?php echo $email; ?></a>
+            <?php endif; ?>
+          </div>
 
-        <div class="typo-subheading-small">
-          <?php if (!empty($address)) : ?>
-            <?php the_field('address', 'option'); ?>
-          <?php endif; ?>
+          <div class="mb-6 text-center typo-subheading">
+            <?php if (!empty($phone)) : ?>
+              <a href="tel:<?php echo $phone; ?>"><?php echo $phone; ?></a>
+            <?php endif; ?>
+          </div>
+
+          <div class="mb-8 text-center sm:mb-0 typo-subheading-medium">
+            <?php if (!empty($address)) : ?>
+              <?php the_field('address', 'option'); ?>
+            <?php endif; ?>
+          </div>
         </div>
       </div>
     </div>
